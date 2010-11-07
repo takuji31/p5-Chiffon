d77bd69e01293561486158779c205a5e57addec6
Nishibayashi Takuji <takuji@senchan.jp>
2010-11-07 21:27:28 +0900

    コードリファレンスをメソッドにエクスポートしてくれるメソッドを追加した

--- lib/Chiffon/Utils.pm
+++ lib/Chiffon/Utils.pm
@@ -1,17 +1,30 @@
 package Chiffon::Utils;
 use Chiffon;
 use parent qw/Exporter/;
+use Carp ();
 
-our @EXPORT = qw/ add_method /;
+our @EXPORT = qw/ add_method add_method_by_coderef /;
 
 sub add_method {
-    my ($class, $target, $method_name) = @_;
-    if(ref ($target)){
+    my ( $class, $target, $method_name ) = @_;
+    if ( ref($target) ) {
         $target = ref($target);
     }
+    my $code = \&$class->$method_name;
+    $class->add_method_by_coderef( $target, $code );
+}
+
+sub add_method_by_coderef {
+    my ( $class, $target, $code ) = @_;
+    if ( ref($target) ) {
+        $target = ref($target);
+    }
+    unless ( ref($code) ne 'CODE' ) {
+        Carp::croak("This is not code reference! $code");
+    }
     {
-        no strict 'refs'; ## no critic
-        *{"$target\::$method_name"} = \&$class->$method_name;
+        no strict 'refs';    ## no critic
+        *{"$target\::$method_name"} = $code;
     }
 }
 
