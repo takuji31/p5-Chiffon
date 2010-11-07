79c95b96f22484124df24f9612673312370e7554
Nishibayashi Takuji <takuji@senchan.jp>
2010-11-07 18:57:45 +0900

    initial commit

---
+++ lib/Chiffon.pm
@@ -0,0 +1,32 @@
+package Chiffon;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Chiffon -
+
+=head1 SYNOPSIS
+
+  use Chiffon;
+
+=head1 DESCRIPTION
+
+Chiffon is
+
+=head1 AUTHOR
+
+Nishibayashi Takuji E<lt>takuji {at} senchan.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
