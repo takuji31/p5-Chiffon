621e9b1e6905cbdd59e97be47c00026fe995f824
Nishibayashi Takuji <takuji@senchan.jp>
2010-11-12 00:18:31 +0900

    Controllerにimport、trigger関連のメソッドとアクセサーを追加

--- lib/Chiffon/Web/Controller.pm
+++ lib/Chiffon/Web/Controller.pm
@@ -1,4 +1,49 @@
 package  Chiffon::Web::Controller;
 use Chiffon;
+use Carp();
 
+sub import {
+    my $class  = shift;
+    my $caller = caller;
+
+    my @methods = qw/
+        new
+        add_before_dispatch add_after_dispatch
+    /;
+}
+
+sub new {
+    my ( $class, $args ) = @_;
+    bless $args,$class;
+}
+
+#trigger
+
+my @triggers = qw/ before_action before_render after_render after_action /;
+
+sub add_trigger {
+    my ( $class, $trigger, $code ) = @_;
+    unless ( grep /$trigger/,@triggers ){
+        Carp::cluck("Trigger $trigger does not exists !");
+    }
+    unless( ref($code) eq 'CODE' ){
+        Carp::cluck('Method add_trigger needs code reference !');
+    }
+    push @{$class->triggers->{$trigger}},$code;
+}
+
+sub call_trigger {
+    my ( $self, $trigger ) = @_;
+    my $codes = $self->triggers->{$trigger} || [];
+
+    for my $code = ( @codes ) {
+        $code->( $self );
+    }
+}
+
+
+sub req      { shift->{req} }
+sub res      { shift->{res} }
+sub request  { shift->{req} }
+sub response { shift->{res} }
 1;
