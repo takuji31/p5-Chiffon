272fca956560429ebc29b602c0850e118662a328
Nishibayashi Takuji <takuji@senchan.jp>
2010-11-11 22:25:46 +0900

    viewを作成するメソッドを追加したりメソッド名をわかりやすくしたりした

--- lib/Chiffon/Web.pm
+++ lib/Chiffon/Web.pm
@@ -10,7 +10,7 @@ sub import {
 
     #Export method
     my @methods
-        = qw/new app env create_dispatcher get_dispatcher dispatch dispatcher
+        = qw/new app env create_dispatcher get_dispatcher create_view get_view dispatch dispatcher
         view container plugin handle_response req res/;
     for my $method (@methods) {
         $class->add_method( $caller, $method );
@@ -20,58 +20,78 @@ sub import {
 
 sub new {
     my ( $class, $args ) = @_;
-    my $self = bless $args, $class;
-    return $self;
+    bless $args, $class;
 }
 
 sub app {
     my $class = shift;
 
-    sub {
+    return sub {
         my $env = shift;
         my $self = $class->new( { env => $env } );
+
+        $self->create_request;
+        $self->create_response;
         $self->create_dispatcher;
-        $self->dispatch;
+        $self->create_view;
+        
+        return $self->dispatch;
     };
 }
 
-sub dispatcher {
-    my $class = shift;
+sub dispatcher_class {
+    my $self = shift;
+    my $class = ref($self) || $self;
     return "$class\::Dispatcher";
 }
 
-sub view { "Chiffon::View::Xslate" }
+sub view_class { "Chiffon::View::Xslate" }
 
-sub plugin {
+sub plugin_classes {
     return [];
 }
 
 sub container {
-    my $class = shift;
+    my $self = shift;
+    my $class = ref($self) || $self;
     $class =~ /^(.+)::[^:]+$/;
     return "$1\::Container";
 }
 
 #Instance methods
 
-sub env      { shift->{env} }
-sub req      { shift->{req} }
-sub res      { shift->{res} }
-
+sub create_request {
+    my $self     = shift;
+    $self->{req} = Chiffon::Web::Request->new( $self->env );
+}
+sub create_response {
+    my $self     = shift;
+    $self->{res} = Chiffon::Web::Response->new;
+}
 sub create_dispatcher {
     my $self       = shift;
-    my $dispatcher = $self->attr->{dispatcher_class}->new({ env => $self->env })
+    my $dispatcher = $self->dispatcher_class->new({ env => $self->env })
         or Carp::croak('Dispatcher not found!');
     $self->{dispatcher} = $dispatcher;
 }
-sub get_dispatcher { shift->{dispatcher} }
+sub create_view {
+    my $self       = shift;
+    my $view = $self->view_class->new({ env => $self->env })
+        or Carp::croak('View not found!');
+    $self->{view} = $view;
+}
+
+sub env { shift->{env} }
+sub req { shift->{req} }
+sub res { shift->{res} }
+sub dispatcher { shift->{dispatcher} }
+sub view { shift->{view} }
 
 sub dispatch {
     my $self = shift;
 
-    $self->{req} = Chiffon::Web::Request->new( $self->env );
     $self->{res} = Chiffon::Web::Response->new;
-    my $dispatch_rule = $self->get_dispatcher->match;
+    my $dispatch_rule = $self->dispatcher->match;
     unless ( $dispatch_rule ) {
         return $self->handle_response('404 Not Found',404);
     }
