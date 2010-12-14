9b91eebd0cd60c127fa97e298ce3c00ba891cbe4
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-14 22:48:35 +0900

    Chiffon::Viewに使ってないメソッドが大量に…

--- lib/Chiffon/View.pm
+++ lib/Chiffon/View.pm
@@ -1,38 +1,5 @@
 package  Chiffon::View;
 use Chiffon;
-
-sub new {
-    my ($class, $args) = @_;
-    
-    $args           ||= +{};
-    $args->{config} ||= +{};
-    $args->{stash}  ||= +{};
-
-    my $self = bless $args,$class;
-    $self->setup_renderer;
-    return $self;
-}
-
 sub render { die 'Abstruct method render !' }
-
-sub setup_renderer { }
-
-sub set_renderer { shift->{renderer} = shift }
-
-sub renderer { shift->{renderer} }
-
-sub stash :lvalue { shift->{stash} }
-
-sub config {
-    my $self = shift;
-    my $class = ref ($self);
-    return $self->{config}->{$class} || +{};
-}
-
-sub app_name {
-    my $self = shift;
-    my $class = ref ($self);
-    return $self->{config}->{app_name} || +{};
-}
 
 1;
