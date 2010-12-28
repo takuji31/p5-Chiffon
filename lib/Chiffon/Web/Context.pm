38c13dfde50bd9f3b7e2a9bccf9fff26dab5c37c
Nishibayashi Takuji <takuji@senchan.jp>
2010-12-28 12:44:25 +0900

    uri_withをContextに生やす（From Kamui）

--- lib/Chiffon/Web/Context.pm
+++ lib/Chiffon/Web/Context.pm
@@ -21,3 +21,26 @@ sub create_session {
     my $self = shift;
     $self->{session} = Plack::Session->new($self->env);
 }
+
+sub uri_with {
+    my($self, $args) = @_;
+    
+    Carp::carp( 'No arguments passed to uri_with()' ) unless $args;
+
+    for my $value (values %{ $args }) {
+        next unless defined $value;
+        for ( ref $value eq 'ARRAY' ? @{ $value } : $value ) {
+            $_ = "$_";
+            utf8::encode( $_ );
+        }
+    };
+    
+    $self->load_class('URI::QueryParam');
+
+    my $uri = $self->req->uri->clone;
+    $uri->query_form( {
+        %{ $uri->query_form_hash },
+        %{ $args },
+    } );
+    return $uri;
+}
