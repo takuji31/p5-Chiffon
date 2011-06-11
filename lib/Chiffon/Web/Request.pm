82096dbd9cc50623abe6772f2f13f30d4bc769be
Nishibayashi Takuji <takuji@senchan.jp>
2011-06-12 03:32:55 +0900

    パラメーターのデコード from Amon2

--- lib/Chiffon/Web/Request.pm
+++ lib/Chiffon/Web/Request.pm
@@ -4,7 +4,67 @@ use warnings;
 
 use parent qw/Plack::Request/;
 
+use Encode ();
+use Hash::MultiValue;
+
 sub is_post_request { $_[0]->method eq 'POST' }
 sub http_host { $_[0]->env->{HTTP_HOST} }
+
+# from Amon2::Request
+sub body_parameters {
+    my ($self) = @_;
+    $self->{'chiffon.body_parameters'} ||= $self->_decode_parameters($self->SUPER::body_parameters());
+}
+
+sub query_parameters {
+    my ($self) = @_;
+    $self->{'chiffon.query_parameters'} ||= $self->_decode_parameters($self->SUPER::query_parameters());
+}
+
+sub _decode_parameters {
+    my ($self, $stuff) = @_;
+
+    my $encoding = Chiffon->context->encoding;
+    my @flatten = $stuff->flatten();
+    my @decoded;
+    while ( my ($k, $v) = splice @flatten, 0, 2 ) {
+        push @decoded, Encode::decode($encoding, $k), Encode::decode($encoding, $v);
+    }
+    return Hash::MultiValue->new(@decoded);
+}
+sub parameters {
+    my $self = shift;
+
+    $self->env->{'chiffon.request.merged'} ||= do {
+        my $query = $self->query_parameters;
+        my $body  = $self->body_parameters;
+        Hash::MultiValue->new( $query->flatten, $body->flatten );
+    };
+}
+
+sub body_parameters_raw {
+    shift->SUPER::body_parameters();
+}
+sub query_parameters_raw {
+    shift->SUPER::query_parameters();
+}
+sub parameters_raw {
+    my $self = shift;
+
+    $self->env->{'plack.request.merged'} ||= do {
+        my $query = $self->SUPER::query_parameters();
+        my $body  = $self->SUPER::body_parameters();
+        Hash::MultiValue->new( $query->flatten, $body->flatten );
+    };
+}
+sub param_raw {
+    my $self = shift;
+
+    return keys %{ $self->parameters_raw } if @_ == 0;
+
+    my $key = shift;
+    return $self->parameters_raw->{$key} unless wantarray;
+    return $self->parameters_raw->get_all($key);
+}
 
 1;
